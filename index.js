52068cfe5334addd7f67bf7830eb023e0b921fbc
Chris Gruber <chris@chrisgruber.com>
2017-02-16 22:00:37 -0600

    Turn off debug logging. Turn on reconnect option

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ if (!process.env.token) {
 
 let controller = Botkit.slackbot({
   stats_optout: true,
-  debug: true
+  debug: false
 });
 
 let bot = controller.spawn({
-  token: process.env.token
+  token: process.env.token,
+  retry: true
 }).startRTM();
 
 controller.hears(['hello (.*)', 'hi (.*)'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
@@ -44,91 +45,91 @@ controller.hears(['hello (.*)', 'hi (.*)'], ['direct_message', 'direct_mention',
 controller.hears(['call me (.*)', 'my name is (.*)'],
   ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
 
-  let name = message.match[1];
+    let name = message.match[1];
 
-  controller.storage.users.get(message.user, function (err, user) {
-    if (!user) {
-      user = {
-        id: message.user
-      };
-    }
+    controller.storage.users.get(message.user, function (err, user) {
+      if (!user) {
+        user = {
+          id: message.user
+        };
+      }
 
-    user.name = name;
+      user.name = name;
 
-    controller.storage.users.save(user, function (err, id) {
-      bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+      controller.storage.users.save(user, function (err, id) {
+        bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+      });
     });
   });
-});
 
 controller.hears(['what is my name', 'who am i'],
   ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
 
-  controller.storage.users.get(message.user, function (err, user) {
-    if (user && user.name) {
-      bot.reply(message, 'Your name is ' + user.name);
-    } else {
-      bot.startConversation(message, function (err, convo) {
-        if (!err) {
-          convo.say('I do not know your name yet!');
-          convo.ask('What should I call you?', function (response, convo) {
-            convo.ask('You want me to call you `' + response.text + '`?', [
-              {
-                pattern: 'yes',
-                callback: function (response, convo) {
-                  // since no further messages are queued after this,
-                  // the conversation will end naturally with status == 'completed'
+    controller.storage.users.get(message.user, function (err, user) {
+      if (user && user.name) {
+        bot.reply(message, 'Your name is ' + user.name);
+      } else {
+        bot.startConversation(message, function (err, convo) {
+          if (!err) {
+            convo.say('I do not know your name yet!');
+            convo.ask('What should I call you?', function (response, convo) {
+              convo.ask('You want me to call you `' + response.text + '`?', [
+                {
+                  pattern: 'yes',
+                  callback: function (response, convo) {
+                    // since no further messages are queued after this,
+                    // the conversation will end naturally with status == 'completed'
 
-                  convo.next();
+                    convo.next();
+                  }
+                },
+                {
+                  pattern: 'no',
+                  callback: function (response, convo) {
+                    // stop the conversation. this will cause it to end with status == 'stopped'
+
+                    convo.stop();
+                  }
+                },
+                {
+                  default: true,
+                  callback: function (response, convo) {
+                    convo.repeat();
+                    convo.next();
+                  }
                 }
-              },
-              {
-                pattern: 'no',
-                callback: function (response, convo) {
-                  // stop the conversation. this will cause it to end with status == 'stopped'
+              ]);
 
-                  convo.stop();
-                }
-              },
-              {
-                default: true,
-                callback: function (response, convo) {
-                  convo.repeat();
-                  convo.next();
-                }
-              }
-            ]);
+              convo.next();
+            }, {'key': 'nickname'}); // store the results in a field called nickname
 
-            convo.next();
-          }, {'key': 'nickname'}); // store the results in a field called nickname
+            convo.on('end', function (convo) {
+              if (convo.status == 'completed') {
+                bot.reply(message, 'OK! I will update my dossier...');
 
-          convo.on('end', function (convo) {
-            if (convo.status == 'completed') {
-              bot.reply(message, 'OK! I will update my dossier...');
+                controller.storage.users.get(message.user, function (err, user) {
+                  if (!user) {
+                    user = {
+                      id: message.user
+                    };
+                  }
 
-              controller.storage.users.get(message.user, function (err, user) {
-                if (!user) {
-                  user = {
-                    id: message.user
-                  };
-                }
+                  user.name = convo.extractResponse('nickname');
 
-                user.name = convo.extractResponse('nickname');
-
-                controller.storage.users.save(user, function (err, id) {
-                  bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                  controller.storage.users.save(user, function (err, id) {
+                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                  });
                 });
-              });
-            } else {
-              // this happens if the conversation ended prematurely for some reason
-              bot.reply(message, 'OK, nevermind!');
-            }
-          });
-        }
-      });
-    }
+              } else {
+                // this happens if the conversation ended prematurely for some reason
+                bot.reply(message, 'OK, nevermind!');
+              }
+            });
+          }
+        });
+      }
+    });
   });
-});
 
 controller.hears(['shutdown'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
   bot.startConversation(message, function (err, convo) {
@@ -162,30 +163,30 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
     let formatUptime = (uptime) => {
       let unit = 'second';
 
-      if(uptime > 60) {
+      if (uptime > 60) {
         uptime = uptime / 60;
         unit = 'minute';
       }
 
-      if(uptime > 60) {
+      if (uptime > 60) {
         uptime = uptime / 60;
         unit = 'hour';
       }
 
       uptime = Math.round(uptime);
 
-      if(uptime > 0) {
-        if(uptime !== 1) {
+      if (uptime > 0) {
+        if (uptime !== 1) {
           unit = unit + 's';
         }
 
         return uptime + ' ' + unit;
       } else {
-        if(unit === 'second') {
+        if (unit === 'second') {
           return 'a few moments';
-        } else if(unit === 'minute') {
+        } else if (unit === 'minute') {
           return 'a few seconds';
-        } else if(unit === 'hour') {
+        } else if (unit === 'hour') {
           return 'a few minutes';
         }
       }
